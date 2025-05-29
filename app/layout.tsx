24edde75dd7cf2df85e3b747d347d4acd67b52b5
Daniel-T-Dada <simplytobs@gmail.com>
2025-05-29 01:31:31 +0100

    temporarily disable dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+// import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { metadata, viewport } from "./metadata";
 import ClientWrapper from "@/components/ClientWrapper";
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> */}
           <SessionProvider>
             <SidebarProvider>
               <ServiceWorkerRegistration />
@@ -44,7 +44,7 @@ export default function RootLayout({
               <Toaster />
             </SidebarProvider>
           </SessionProvider>
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </body>
     </html>
   );
